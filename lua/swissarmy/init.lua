4fadea41f2acccf49b418bb0ed7834035b82513b
Chris Taylor <chris@christaylordeveloper.co.uk>
2025-02-17 11:34:27 +0300

    Comment out a function.

--- lua/swissarmy/init.lua
+++ lua/swissarmy/init.lua
@@ -15,13 +15,13 @@ end
 M.setup = function(opts)
 	opts = opts or {}
 
-	vim.keymap.set("n", "<Leader>h", function()
-		if opts.name then
-			print("hello, " .. opts.name)
-		else
-			print("hello")
-		end
-	end)
+	-- vim.keymap.set("n", "<Leader>h", function()
+	-- 	if opts.name then
+	-- 		print("hello, " .. opts.name)
+	-- 	else
+	-- 		print("hello")
+	-- 	end
+	-- end)
 end
 
 return M
